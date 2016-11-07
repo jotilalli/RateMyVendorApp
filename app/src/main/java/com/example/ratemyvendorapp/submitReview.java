1a68f4ae97edcc2629ffa5affde56f77a6c3b76c
jotilalli <jotilalli@gmail.com>
2016-11-06 23:02:35 -0800

    submit review page complete. pre-navigation drawer commit

---
+++ app/src/main/java/com/example/ratemyvendorapp/submitReview.java
@@ -0,0 +1,13 @@
+package com.example.ratemyvendorapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class submitReview extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_submit_review);
+    }
+}
